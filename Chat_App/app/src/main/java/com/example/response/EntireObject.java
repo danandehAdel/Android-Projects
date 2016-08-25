b60a431c13682b6c08ad17db29b4d884dca7c68e
Adel Danandeh <adel.danandeh@gmail.com>
2016-08-24 23:54:51 -0700

    Adding Chat App
    Adding the Chat Application

---
+++ Chat_App/app/src/main/java/com/example/response/EntireObject.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class EntireObject {
+
+        @SerializedName("response")
+        @Expose
+        public HTTPResponse response;
+
+}
---
+++ Chat_App/app/src/main/java/com/example/response/HTTPResponse.java
@@ -0,0 +1,14 @@
+package com.example;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class HTTPResponse {
+    @SerializedName("conditions")
+    @Expose
+    public Conditions conditions;
+    @SerializedName("result")
+    @Expose
+    public String result;
+}
